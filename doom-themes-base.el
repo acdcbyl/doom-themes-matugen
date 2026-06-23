6f498332303cbc8b7ef0a288cf8dd5e56c487d23
AISER <47445389+acdcbyl@users.noreply.github.com>
2026-06-23 15:27:53 +0800

    Change background color for selected modified tabs

--- doom-themes-base.el
+++ doom-themes-base.el
@@ -294,7 +294,7 @@
     ((centaur-tabs-default &inherit tab-bar) :box nil)
     ((centaur-tabs-selected &inherit tab-bar-tab) :box nil)
     ((centaur-tabs-unselected &inherit tab-bar-tab-inactive) :box nil)
-    (centaur-tabs-selected-modified   :background bg :foreground teal)
+    (centaur-tabs-selected-modified   :background bg-bright :foreground teal)
     (centaur-tabs-unselected-modified :background bg-alt :foreground teal)
     (centaur-tabs-active-bar-face :background (if (bound-and-true-p -modeline-bright) modeline-bg highlight))
     (centaur-tabs-modified-marker-selected
